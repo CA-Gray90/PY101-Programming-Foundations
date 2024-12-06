35110a69994263945c649cad2c9c0fc05cbc029d
Charles <cgray888@gmail.com>
2024-12-06 11:26:54 +1100

    Added a prompt function adding '==>' to each prompt for input.

--- lesson_2/calculator/calculator.py
+++ lesson_2/calculator/calculator.py
@@ -1,21 +1,20 @@
-print('Welcome to Calculator!')
+def prompt(message):
+    print(f'==> {message}')
 
-# Ask the user for the first number
-print("What's the first number?")
+prompt('Welcome to Calculator!')
+
+prompt("What's the first number?")
 num_1 = input()
 
-# Ask the user for the second number
-print("What's the second number?")
+prompt("What's the second number?")
 num_2 = input()
 
-# Ask the user for an operation to perform
-print(
+prompt(
     ('What operation would you like to perform?\n'
-     '1 - Addition, 2 - Subtraction, 3 - Multiplication, 4 - Division')
+    '1 - Addition, 2 - Subtraction, 3 - Multiplication, 4 - Division')
 )
 operation = input()
 
-# perform the calculation
 if operation == '1':
     result = int(num_1) + int(num_2)
 elif operation == '2':
@@ -25,5 +24,4 @@ elif operation == '3':
 elif operation == '4':
     result = int(num_1) / int(num_2)
 
-# Print out the result
-print(f'The result is {result}')
+prompt(f'The result is {result}')
