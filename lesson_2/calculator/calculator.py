531272bdd9dc1913c05663674b7c066ef7b94fca
Charles <cgray888@gmail.com>
2024-12-06 11:33:22 +1100

    Changed if/else in calculator.py to match/case.

--- lesson_2/calculator/calculator.py
+++ lesson_2/calculator/calculator.py
@@ -11,17 +11,18 @@ num_2 = input()
 
 prompt(
     ('What operation would you like to perform?\n'
-    '1 - Addition, 2 - Subtraction, 3 - Multiplication, 4 - Division')
+    '1) Addition, 2) Subtraction, 3) Multiplication, 4) Division')
 )
 operation = input()
 
-if operation == '1':
-    result = int(num_1) + int(num_2)
-elif operation == '2':
-    result = int(num_1) - int(num_2)
-elif operation == '3':
-    result = int(num_1) * int(num_2)
-elif operation == '4':
-    result = int(num_1) / int(num_2)
+match operation:
+    case '1':
+        result = int(num_1) + int(num_2)
+    case '2':
+        result = int(num_1) - int(num_2)
+    case '3':
+        result = int(num_1) * int(num_2)
+    case '4':
+        result = int(num_1) / int(num_2)
 
 prompt(f'The result is {result}')
