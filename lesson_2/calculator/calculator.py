8312d5459c8f88fe7349a8ee6616a7fe8a524847
Charles <cgray888@gmail.com>
2024-12-06 14:52:57 +1100

    Added functionality to check if invalid number and a check for valid operation.

--- lesson_2/calculator/calculator.py
+++ lesson_2/calculator/calculator.py
@@ -1,19 +1,37 @@
 def prompt(message):
     print(f'==> {message}')
 
+def invalid_num(number_str):
+    try:
+        int(number_str)
+    except ValueError:
+        return True
+    return False
+
 prompt('Welcome to Calculator!')
 
 prompt("What's the first number?")
 num_1 = input()
 
+while invalid_num(num_1):   # Checks for valid input, loops back if invalid
+    prompt('Invalid input entered, try again:')
+    num_1 = input()
+
 prompt("What's the second number?")
 num_2 = input()
 
-prompt(
-    ('What operation would you like to perform?\n'
-    '1) Addition, 2) Subtraction, 3) Multiplication, 4) Division')
-)
+while invalid_num(num_2):
+    prompt('Invalid input entered, try again:')
+    num_2 = input()
+
+prompt('''What operation would you like to perform?
+1) Addition, 2) Subtraction, 3) Multiplication, 4) Division''')
+
 operation = input()
+
+while operation not in ['1', '2', '3', '4']:
+    prompt('Invalid operation chosen, must choose 1, 2, 3 or 4')
+    operation = input()
 
 match operation:
     case '1':
