a4c03daa4a12b7740478cbef8afe20085c87944e
Charles <cgray888@gmail.com>
2025-01-06 16:18:54 +1100

    Added a function to loop back and use the calculator again

--- lesson_2/calculator/calculator.py
+++ lesson_2/calculator/calculator.py
@@ -1,3 +1,4 @@
+# FUNCTIONS
 def prompt(message):
     print(f'==> {message}')
 
@@ -8,39 +9,57 @@ def invalid_num(number_str):
         return True
     return False
 
+def try_again(answer):
+    answer = answer.casefold()
+    while True:
+        if answer == 'y':
+            return True
+        if answer == 'n':
+            return False
+        else:
+            prompt("Invalid response, please enter either 'y' or 'n':")
+            answer = input()
+
+# PROGRAM START
 prompt('Welcome to Calculator!')
 
-prompt("What's the first number?")
-num_1 = input()
-
-while invalid_num(num_1):   # Checks for valid input, loops back if invalid
-    prompt('Invalid input entered, try again:')
+while True:
+    prompt("What's the first number?")
     num_1 = input()
 
-prompt("What's the second number?")
-num_2 = input()
+    while invalid_num(num_1):   # Validity Check
+        prompt('Invalid input entered, try again:')
+        num_1 = input()
 
-while invalid_num(num_2):
-    prompt('Invalid input entered, try again:')
+    prompt("What's the second number?")
     num_2 = input()
 
-prompt('''What operation would you like to perform?
-1) Addition, 2) Subtraction, 3) Multiplication, 4) Division''')
+    while invalid_num(num_2):
+        prompt('Invalid input entered, try again:')
+        num_2 = input()
 
-operation = input()
-
-while operation not in ['1', '2', '3', '4']:
-    prompt('Invalid operation chosen, must choose 1, 2, 3 or 4')
+    prompt('''What operation would you like to perform?
+    (1) Addition, (2) Subtraction, (3) Multiplication, (4) Division''')
     operation = input()
 
-match operation:
-    case '1':
-        result = int(num_1) + int(num_2)
-    case '2':
-        result = int(num_1) - int(num_2)
-    case '3':
-        result = int(num_1) * int(num_2)
-    case '4':
-        result = int(num_1) / int(num_2)
+    while operation not in ['1', '2', '3', '4']:
+        prompt('Invalid operation chosen, must choose 1, 2, 3 or 4. Try again:')
+        operation = input()
 
-prompt(f'The result is {result}')
+    match operation:
+        case '1':
+            result = int(num_1) + int(num_2)
+        case '2':
+            result = int(num_1) - int(num_2)
+        case '3':
+            result = int(num_1) * int(num_2)
+        case '4':
+            result = int(num_1) / int(num_2)
+
+    prompt(f'The result is {result}')
+
+    prompt('Would you like to try again? y/n')
+    user_answer = input()
+    if not try_again(user_answer):
+        prompt('Exiting Program...')
+        break
