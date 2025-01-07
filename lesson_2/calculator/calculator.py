04e0c2d084049369e2dbc80b45cf24eb33c8be26
Charles <cgray888@gmail.com>
2025-01-07 14:05:05 +1100

    Changed the prompt function in calculator.py to call values from calculator_messages.json from within the function itself

--- lesson_2/calculator/calculator.py
+++ lesson_2/calculator/calculator.py
@@ -2,11 +2,14 @@
 import json
 
 with open('calculator_messages.json', 'r') as file:
-    messages = json.load(file)
+    MESSAGES = json.load(file)
 
 # FUNCTIONS
-def prompt(message):
-    print(f'==> {message}')
+def prompt(message, index=None):
+    if index is not None:
+        print(f'==> {MESSAGES[LANG][message][index]}')
+    else:
+        print(f'==> {MESSAGES[LANG][message]}')
 
 def invalid_num(number_str):
     try:
@@ -22,11 +25,11 @@ def try_again(answer):
             return True
         if answer == 'n':
             return False
-        prompt(MESSAGES['invalid_try_again'])
+        prompt('invalid_try_again')
         answer = input()
 
 def language_select():
-    prompt("Please select a language: "
+    print("Please select a language: "
            "'en' for English, 'au' for Australian")
     answer = input().casefold()
 
@@ -35,38 +38,35 @@ def language_select():
             return 'en'
         if answer in ['au', 'australian']:
             return 'au'
-        prompt("Invalid input, please enter either 'en' or 'au' ")
+        print("Invalid input, please enter either 'en' or 'au' ")
         answer = input().casefold()
 
 # PROGRAM START
 LANG = language_select()
 
-MESSAGES = messages[LANG]
-
-prompt(MESSAGES['greeting'])
-
+prompt('greeting')
 
 while True:
-    prompt(MESSAGES['num_prompt'][0])
+    prompt('num_prompt', 0)
     num_1 = input()
 
     while invalid_num(num_1):   # Validity Check
-        prompt(MESSAGES['invalid_num_message'])
+        prompt('invalid_num_message')
         num_1 = input()
 
-    prompt(MESSAGES['num_prompt'][1])
+    prompt('num_prompt', 1)
     num_2 = input()
 
     while invalid_num(num_2):
-        prompt(MESSAGES['invalid_num_message'])
+        prompt('invalid_num_message')
         num_2 = input()
 
-    prompt(MESSAGES['operation_message'])
-    prompt(MESSAGES['operation_options'])
+    prompt('operation_message')
+    prompt('operation_options')
     operation = input()
 
     while operation not in ['1', '2', '3', '4']:    # Operation validity check
-        prompt(MESSAGES['invalid_operation_message'])
+        prompt('invalid_operation_message')
         operation = input()
 
     match operation:
@@ -80,16 +80,17 @@ while True:
             if num_2 != '0':    # Check for division by Zero
                 result = int(num_1) / int(num_2)
             else:
-                print(MESSAGES['division_by_zero'])
+                prompt('division_by_zero')
                 result = None
 
     if result is not None:
-        prompt(f'{MESSAGES['result_message']} {result}')
+        prompt('result_message')
+        print(f'{result}')
 
-    prompt(MESSAGES['try_again_message'])
+    prompt('try_again_message')
     user_answer = input()
     if try_again(user_answer):
-        prompt(MESSAGES['try_again_answer'][0])
+        prompt('try_again_answer', 0)
     else:
-        prompt(MESSAGES['try_again_answer'][1])
+        prompt('try_again_answer', 1)
         break
