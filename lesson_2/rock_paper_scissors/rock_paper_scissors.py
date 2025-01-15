2373a95a1b1517d10b162df15dc754c89d8c8bdf
Charles <cgray888@gmail.com>
2025-01-15 16:03:38 +1100

    rock_paper_scissors.py: Cleaned up code a little

--- lesson_2/rock_paper_scissors/rock_paper_scissors.py
+++ lesson_2/rock_paper_scissors/rock_paper_scissors.py
@@ -8,7 +8,7 @@ VALID_CHOICES_DICT = {
     'sp': 'spock'
 }
 
-VALID_CHOICES = ", ".join(list(f'"{key}" for {value}' 
+VALID_CHOICES = ", ".join(list(f'"{key}" for {value}'
                            for (key, value) in VALID_CHOICES_DICT.items()))
 
 def prompt(message):
@@ -16,36 +16,35 @@ def prompt(message):
 
 def get_winner(player, computer):
     if ((player == 'rock' and
-         (computer == 'scissors' or computer == 'lizard')) or
+         computer in ('scissors', 'lizard')) or
         (player == 'paper' and
-         (computer == 'rock' or computer == 'spock')) or
+         computer in ('rock', 'spock')) or
         (player == 'scissors' and
-         (computer == 'paper' or computer == 'lizard')) or
+         computer in ('paper', 'lizard')) or
         (player == 'lizard' and
-         (computer == 'spock' or computer == 'paper')) or
+         computer in ('spock', 'paper')) or
         (player == 'spock' and
-         (computer == 'scissors' or computer == 'rock'))):
+         computer in ('scissors', 'rock'))):
         return 'player'
-    
-    elif ((player == 'scissors' and
-           (computer == 'rock' or computer == 'spock')) or
+
+    if ((player == 'scissors' and
+           computer in ('rock', 'spock')) or
         (player == 'rock' and
-         (computer == 'paper' or computer == 'spock')) or
+         computer in ('paper', 'spock')) or
         (player == 'paper' and
-         (computer == 'scissors' or computer == 'lizard')) or
-         (player == 'lizard' and
-          (computer == 'scissors' or computer == 'rock')) or
-          (player == 'spock' and
-           (computer == 'paper' or computer == 'lizard'))):
+         computer in ('scissors', 'lizard')) or
+        (player == 'lizard' and
+         computer in ('scissors', 'rock')) or
+        (player == 'spock' and
+         computer in ('paper', 'lizard'))):
         return 'computer'
-    
-    else:
-        return None
-    
+
+    return None
+
 def get_player_choice():
     prompt('Choose one: '
            f'{VALID_CHOICES}')
-    
+
     choice = input().lower()
     return choice
 
@@ -61,13 +60,12 @@ def de_abbreviate_choice(choice):
     return choice
 
 def get_computer_choice():
-    choice = random.choice([value for value\
-                                     in VALID_CHOICES_DICT.values()])
+    choice = random.choice(list(VALID_CHOICES_DICT.values()))
     return choice
 
-def get_grand_winner(list):
-    for player in list:
-        if list.count(player) == 3:
+def get_grand_winner(winner_list):
+    for player in winner_list:
+        if winner_list.count(player) == 3:
             return player
     return None
 
@@ -91,11 +89,11 @@ while main_game_continue:
         while invalid_choice(player_choice):
             prompt("Invalid choice, please try again\n"
                 f"Your choices are: {VALID_CHOICES}")
-            
+
             player_choice = input()
-        
+
         player_choice = de_abbreviate_choice(player_choice)
-        
+
         computer_choice = get_computer_choice()
 
         prompt(f'You chose {player_choice}, computer chose {computer_choice}.')
@@ -127,7 +125,7 @@ while main_game_continue:
     if answer[0] == 'n':
         main_game_continue = False
 
-# TODO: 
+# TODO:
 # display score after each match
 # display which match is about to commence
 # add time delays to make the game more interesting
