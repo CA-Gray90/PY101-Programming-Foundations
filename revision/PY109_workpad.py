9f36f6bcd2194c2233766e4b05cc0278f1d8577a
Charles <cgray888@gmail.com>
2025-04-18 09:59:23 +1000

    Small changes made to files in revision for study and assessment prep

--- revision/PY109_workpad.py
+++ revision/PY109_workpad.py
@@ -1,21 +1,48 @@
-everest = "Everest"
-kilimanjaro = "Kilimanjaro"
-fuji = "Fuji"                   # Assign global variables to string objects
+def double_odd_indices(lst):
+    return [num * 2 if idx % 2 == 1 else num for idx, num in enumerate(lst)]
 
-mountain_list = [everest, kilimanjaro, fuji]    # Assign `mountain_list` to list containing variables defined in line 1 - 3.
+# PCODE:
+# List comprehension
 
-# print(mountain_list)
-# print(repr(mountain_list[0]))
+# result = []
+# LOOP through elements of list
+# for each element, check if odd:
+#   if odd, element * 2, append this to the list.
+# else:
+#   append element to list as is
+# 
+# return new list
 
-for idx in range(len(mountain_list)):
-    mountain_list[idx] += " " + str(len(mountain_list[idx]))    # Mutating the list object, changing the values at each index.
-                                                                # mountain_list[idx] = mountain_list[idx] + " " + str(len(mountain_list[idx]))
-                                                                # mountain_list[0] = 'Everest' + " " + '7'
+print(double_odd_indices([1, 2, 3, 4, 5])  == [1, 4, 3, 8, 5])
+print(double_odd_indices([10, 20, 30, 40]) == [10, 40, 30, 80])
+print(double_odd_indices([]) == [])
 
-                                                                # We are mutating a list object using square bracket notation and indexing, changing the elements
+# Write a function filter_by_age that takes a dictionary similar to the munsters
+# dictionary from the curriculum and returns a new dictionary containing only
+# family members who are older than a specified age.
 
-print(mountain_list) # ['Everest 7', 'Kilimanjaro 11', 'Fuji 4']
-# print(everest, kilimanjaro, fuji)
-print(id(everest))
-everest += "is the highest mountain"
-print(id(everest))
+def filter_by_age(dictionary, age_limit):
+    result_dict = {}
+
+    for name in dictionary.keys():
+        age = dictionary[name]['age']
+        if age >= age_limit:
+            result_dict.update({name : dictionary[name]})
+    
+    return result_dict
+
+# Write a function called merge_dicts that takes two dictionaries and returns
+# a new dictionary. If both dictionaries have the same key, the value in the
+# returned dictionary should be the sum of the values if they are numbers, or
+# the concatenation if they are strings.
+
+def merge_dicts(dict_1, dict_2):
+    result = dict_1.copy()
+
+    for key in dict_2:
+        if key in result.keys():
+            result[key] += dict_2[key]
+        else:
+            result[key] = dict_2[key]
+    
+    return result
